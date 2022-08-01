84a54915f87b47026625420fc3cfbb2cd72a84af
Ryo Somatani <ryo_somatani@outlook.com>
2022-08-01 02:43:59 -0700

    feat: add exception handling

---
+++ src/main/java/org/kutaka/adminapi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package org.kutaka.adminapi.exception;
+
+import java.time.LocalDateTime;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(MissingRequestParameterException.class)
+  public ResponseEntity<?> handleMissingRequestParameterException(
+      MissingRequestParameterException exception, WebRequest request) {
+    ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), HttpStatus.BAD_REQUEST,
+        exception.getMessage(),
+        request.getDescription(false));
+    return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(AlreadyExistsException.class)
+  public ResponseEntity<?> handleAlreadyExistsException(
+      AlreadyExistsException exception, WebRequest request) {
+    ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), HttpStatus.BAD_REQUEST,
+        exception.getMessage(),
+        request.getDescription(false));
+    return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<?> handleGeneralException(Exception exception, WebRequest request) {
+    ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), HttpStatus.INTERNAL_SERVER_ERROR,
+        exception.getMessage(),
+        request.getDescription(false));
+    return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+}

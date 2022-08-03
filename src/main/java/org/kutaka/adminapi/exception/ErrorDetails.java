36ee063c775e5c034c31535e06d9ad0c36552023
Ryo Somatani <ryo_somatani@outlook.com>
2022-08-03 00:17:26 -0700

    refactor: add lombok and omit constructors, getters and setters

--- src/main/java/org/kutaka/adminapi/exception/ErrorDetails.java
+++ src/main/java/org/kutaka/adminapi/exception/ErrorDetails.java
@@ -2,51 +2,17 @@ package org.kutaka.adminapi.exception;
 
 import java.time.LocalDateTime;
 import org.springframework.http.HttpStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+@AllArgsConstructor
+@Getter
+@Setter
 public class ErrorDetails {
 
   private LocalDateTime timestamp;
   private HttpStatus status;
   private String message;
   private String details;
-
-  public ErrorDetails(LocalDateTime timestamp, HttpStatus status, String message, String details) {
-    this.timestamp = timestamp;
-    this.status = status;
-    this.message = message;
-    this.details = details;
-  }
-
-  public LocalDateTime getTimestamp() {
-    return timestamp;
-  }
-
-  public void setTimestamp(LocalDateTime timestamp) {
-    this.timestamp = timestamp;
-  }
-
-  public HttpStatus getStatus() {
-    return status;
-  }
-
-  public void setStatus(HttpStatus status) {
-    this.status = status;
-  }
-
-  public String getMessage() {
-    return message;
-  }
-
-  public void setMessage(String message) {
-    this.message = message;
-  }
-
-  public String getDetails() {
-    return details;
-  }
-
-  public void setDetails(String details) {
-    this.details = details;
-  }
-
 }
--- src/main/java/org/kutaka/adminapi/model/Novel.java
+++ src/main/java/org/kutaka/adminapi/model/Novel.java
@@ -3,8 +3,14 @@ package org.kutaka.adminapi.model;
 import java.util.Date;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Document("novels")
+@RequiredArgsConstructor
+@Getter
+@Setter
 public class Novel {
 
   @Id
@@ -25,90 +31,4 @@ public class Novel {
   private Date createdAt;
 
   private Date updatedAt;
-
-  public Novel(String id, Boolean isHidden, String nameEn, String nameJa, Integer order, Object categories, Object tags,
-      Date createdAt, Date updatedAt) {
-    this.id = id;
-    this.isHidden = isHidden;
-    this.nameEn = nameEn;
-    this.nameJa = nameJa;
-    this.order = order;
-    this.categories = categories;
-    this.tags = tags;
-    this.createdAt = createdAt;
-    this.updatedAt = updatedAt;
-  }
-
-  public String getId() {
-    return id;
-  }
-
-  public Boolean getIsHidden() {
-    return isHidden;
-  }
-
-  public void setIsHidden(Boolean isHidden) {
-    this.isHidden = isHidden;
-  }
-
-  public void setId(String id) {
-    this.id = id;
-  }
-
-  public String getNameEn() {
-    return nameEn;
-  }
-
-  public void setNameEn(String nameEn) {
-    this.nameEn = nameEn;
-  }
-
-  public String getNameJa() {
-    return nameJa;
-  }
-
-  public void setNameJa(String nameJa) {
-    this.nameJa = nameJa;
-  }
-
-  public Integer getOrder() {
-    return order;
-  }
-
-  public void setOrder(Integer order) {
-    this.order = order;
-  }
-
-  public Object getCategories() {
-    return categories;
-  }
-
-  public void setCategories(Object categories) {
-    this.categories = categories;
-  }
-
-  public Object getTags() {
-    return tags;
-  }
-
-  public void setTags(Object tags) {
-    this.tags = tags;
-  }
-
-  public Date getCreatedAt() {
-    return createdAt;
-  }
-
-  public void setCreatedAt(Date createdAt) {
-    this.createdAt = createdAt;
-  }
-
-  public Date getUpdatedAt() {
-    return updatedAt;
-  }
-
-  public void setUpdatedAt(Date updatedAt) {
-    this.updatedAt = updatedAt;
-  }
-
 }
